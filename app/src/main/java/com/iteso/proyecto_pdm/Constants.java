526ed13e7273bec5faa08914a9a8b1b0d67056ed
Cursos LABDESI <Cursos@T20409.ds.iteso.mx>
2019-05-12 19:44:27 -0500

    cambios camara terminada

--- app/src/main/java/com/iteso/proyecto_pdm/Constants.java
+++ app/src/main/java/com/iteso/proyecto_pdm/Constants.java
@@ -3,5 +3,9 @@ package com.iteso.proyecto_pdm;
 public class Constants {
     public final static String MAIN_PACKAGE = "com.iteso.tarea4.PREFERENCES";
     public final static String TOKEN_PREFERENCE = "TOKEN";
+    public static final String FOTO_DE_FRENTE = "Foto de Frente";
+    public static final String FOTO_DE_VUELTA = "Foto de Vuelta";
+    public static final String FOTO_EN_MANO = "Foto en Mano";
+    public static final String COM_ITESO_PROYECTO_PDM_FOTOS = "com.iteso.proyecto_pdm.FOTOS";
 
 }
